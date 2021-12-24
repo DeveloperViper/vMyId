6a2717b1228f439360fa532773d8450a2f157892
fatbo <fatbo@DESKTOP-DS7HSU7>
2021-12-24 15:09:28 -0500

    Add project files.

---
+++ Playerid/client.cs
@@ -0,0 +1,23 @@
+﻿using CitizenFX.Core;
+using System;
+using static CitizenFX.Core.Native.API;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playerid
+{
+    public class Main : BaseScript
+    {
+        public void MyidCommand(int source, List<object> args, string raw)
+        {
+            TriggerEvent("chat:addMessage", new 
+            {
+                color = new[] { 0, 0, 255 },
+                multiline = true,
+                args = new[] { source }
+            });
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,22 @@
+﻿using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using System;
+using System.Collections.Generic;
+
+namespace Server
+{
+	public class Server : BaseScript
+	{
+        [Command("myid")]
+        public void MyidCommand(int source, List<object> args, string raw)
+        {
+            TriggerClientEvent(Players[source], "chat:addMessage", new
+            {
+                color = new[] { 0, 0, 255 },
+                multiline = true,
+                args = new[] { source }
+            });
+        }
+    }
+}
+
